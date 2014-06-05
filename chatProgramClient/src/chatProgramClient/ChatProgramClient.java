4a4f633a33a5acd45541b1fb24649030ff0605b8
Ryan Milem <gint271@gmail.com>
2014-06-05 11:51:29 -0700

    Started making connection to server.  Copied other classes from server project.

--- chatProgramClient/src/chatProgramClient/ChatProgramClient.java
+++ chatProgramClient/src/chatProgramClient/ChatProgramClient.java
@@ -1,9 +1,26 @@
 package chatProgramClient;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
 public class ChatProgramClient {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		Socket server;
+		BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+		
+		System.out.println("Enter the IP address or hostname of the server.");
+		try
+		{
+			server = new Socket(keyboard.readLine(), 1201);
+		}
+		Catch (Exception e)
+		{
+			
+		}
+		
 
 	}
 
