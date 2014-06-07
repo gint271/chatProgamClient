53983dd9b09d4f860cb6a8dbb9b248e5ca7f911f
Ryan Milem <gint271@gmail.com>
2014-06-06 20:15:15 -0700

    Final version.

--- chatProgramClient/src/chatProgramClient/ChatProgramClient.java
+++ chatProgramClient/src/chatProgramClient/ChatProgramClient.java
@@ -8,19 +8,46 @@ public class ChatProgramClient {
 
 	public static void main(String[] args) 
 	{
-		Socket server;
+		ChatFrame frame = null;
+		Socket server = null;
 		BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+		String username = null;
+		
+		System.out.println("What is your username?" );
+		try
+		{
+			username = keyboard.readLine();
+		}
+		catch (Exception e)
+		{
+			System.out.println("Failed to read username.");
+			System.exit(2);
+		}
 		
 		System.out.println("Enter the IP address or hostname of the server.");
 		try
 		{
 			server = new Socket(keyboard.readLine(), 1201);
 		}
-		Catch (Exception e)
+		catch (Exception e)
 		{
-			
+			System.out.println("Failed to connect to server.");
+			System.exit(2);
 		}
 		
+		try 
+		{
+			frame = new ChatFrame(server, username);
+			frame.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		
+		
+		//Begins the input and output threads.
+		//new Thread(new writeThread(newSocket, frame)).start();
+		new Thread(new readThread(server, frame)).start();
 
 	}
 
--- chatProgramClient/src/chatProgramClient/readThread.java
+++ chatProgramClient/src/chatProgramClient/readThread.java
@@ -1,5 +1,4 @@
 package chatProgramClient;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.Socket;
